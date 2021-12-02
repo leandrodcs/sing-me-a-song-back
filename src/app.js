81f02ce2b2d6c18ebdaa6d11e66aac683b7826b1
leandrodcs <eng.leandrodcs@gmail.com>
2021-12-02 15:48:12 -0300

    Simple post recommendation function created

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import cors from 'cors';
 
+import * as recommendationController from './controllers/recommendationController.js';
+
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+app.post('/recommendations', recommendationController.postRecommendation);
 
 app.get('/health', (req, res) => {
     res.status(200).send('Server is up.');
---
+++ src/controllers/recommendationController.js
@@ -0,0 +1,7 @@
+async function postRecommendation() {
+    
+}
+
+export {
+    postRecommendation,
+}
